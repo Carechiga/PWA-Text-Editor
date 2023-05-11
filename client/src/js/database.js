17386489de5f89ac3099957d0c9cf953738ba352
Carechiga <Carechiga43@gmail.com>
2023-05-11 10:51:46 -0700

    added some comments to code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,21 +12,25 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
+  //this creates a new connection to the database and gives it a name and a version number
   const jateDb = await openDB('jate' , 1);
-  const tx = jateDb.transaction('jate', 'readwrite');
+  //this creates a new transaction and sets the priveliges of the database in this case read and write priveliges
+  const tx = jateDb.transaction('jate', 'readwrite')
   const store = tx.objectStore('jate');
+  //put method updates content of sote
   const request = store.put({jate: content});
   const result = await request;
   console.log("Data added to database!", result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
+   //this creates a new connection to the database and gives it a name and a version number
   const jateDb= await openDB('jate', 1);
+  //this creates a new transaction and sets the priveliges of the database in this case read only priveliges
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
+  //get all methor gets content from db
   const request = store.getAll();
   const result= await request;
   console.log(result);
